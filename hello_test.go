34cfb141f5954ae22c9594cb8a6bfdbdfad1e550
Raghul <raviraghul3016@gmail.com>
2025-01-31 08:23:37 +0530

    helper func for test added

--- hello_test.go
+++ hello_test.go
@@ -6,23 +6,22 @@ func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 
 		got := Hello("Raghul")
-
 		want := "Hello, Raghul!"
-
-		if got != want {
-			t.Errorf("got %q and wanted %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying hello without parameters", func(t *testing.T) {
 
 		got := Hello("")
 		want := "Hello, Bangaari!"
-
-		if got != want {
-			t.Errorf("got %q and wanted %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q and wanted %q", got, want)
+	}
 }
